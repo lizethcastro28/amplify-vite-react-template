44cc8461cb22ef965ec80dbef1bb20475b3af3b6
Lizeth Castro <lcastro@MacBook-Air-de-Lizeth.local>
2024-08-08 15:27:48 -0400

    createSessionLivenes en un archivo separa

--- amplify/functions/api-function/handler.ts
+++ amplify/functions/api-function/handler.ts
@@ -1,6 +1,7 @@
 import type { APIGatewayProxyHandler, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 import * as AWS from 'aws-sdk';
 import { getFaceLivenessSession } from './getFaceLivenessSession'; 
+import { createSessionLiveness } from './createSessionLiveness';
 
 const rekognition = new AWS.Rekognition();
 
@@ -22,54 +23,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayEvent): P
     }
 
     return response;
-};
-
-// Función para manejar solicitudes POST
-const createSessionLiveness = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-    console.log('-----------createSessionLiveness------')
-    try {
-        const clientRequestToken = event.requestContext.requestId;
-
-        const params = {
-            ClientRequestToken: clientRequestToken, // Opcional pero recomendado para idempotencia
-            Settings: { // Opcional, para configurar el almacenamiento de imágenes y el límite de imágenes de auditoría
-                AuditImagesLimit: 4, // Puedes especificar de 0 a 4
-                OutputConfig: {
-                    S3Bucket: 'video-signature3-images',
-                    S3KeyPrefix: 'liveness-sessions/'
-                }
-            }
-        };
-
-        const session = await rekognition.createFaceLivenessSession(params).promise();
-
-        return {
-            statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*"
-            },
-            body: JSON.stringify(session)
-        };
-    } catch (error: unknown) {
-        console.error('Error creating liveness session:', error);
-
-        let errorMessage = 'Unknown error';
-        if (error instanceof Error) {
-            errorMessage = error.message;
-        } else if (typeof error === 'string') {
-            errorMessage = error;
-        }
-
-        return {
-            statusCode: 500,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*"
-            },
-            body: JSON.stringify({ error: errorMessage })
-        };
-    }
 };
 
 // Función para manejar solicitudes desconocidas
